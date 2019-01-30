141cd5a9bed2367b9f0267996e766eaa472e3325
Mc-fly82 <flavius_marc@yahoo.fr>
2019-01-30 11:23:39 +0100

    add clock to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import Clock from 'react-live-clock';
 //import ReactDOM from 'react-dom'
 import './App.css'
-import {Board} from "./Board"
+import {Board}              from "./Board"
 
 class App extends Component {
   render() {
@@ -9,10 +10,14 @@ class App extends Component {
       <div id="App">
         <header id="App-header">
             <h1 style={{"textAlign":"center"}}>Board de Pierre Sindin</h1>
+
+
+            <div id={"Date"}><Clock format={'HH:mm:ss'} ticking={true}  /></div>
         </header>
           <Board/>
       </div>
     )
+
   }
 }
 export default App
